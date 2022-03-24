a94a833a3a09c5b7d16224f322f619c9869a740e
yeasin50 <yeasinsheikh50@gmail.com>
2022-03-24 17:25:55 +0600

    startUp-project

--- app/src/main/java/com/example/androidstudiolab/MainActivity.java
+++ app/src/main/java/com/example/androidstudiolab/MainActivity.java
@@ -8,28 +8,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText firstNameEditText, lastNameEditText;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        firstNameEditText = findViewById(R.id.firstNameEditTextId);
-        lastNameEditText = findViewById(R.id.lastNameEditTextId);
 
     }
 
-    public void submitFunction(View view) {
-        /// get user fistName from `EditText`
-        final String firstName = firstNameEditText.getText().toString();
-
-        /// get user lastName from `EditText`
-        final String lastName = lastNameEditText.getText().toString();
-        Toast.makeText(this, "Thank you " + firstName + " " + lastName + "!", Toast.LENGTH_LONG).show();
-
-        //clear the editText text
-        firstNameEditText.setText(null);
-        lastNameEditText.setText(null);
-    }
 }
