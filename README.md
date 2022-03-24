30b1b2904cfae35f67ef94c7f19d4bed67aede02
yeasin50 <yeasinsheikh50@gmail.com>
2022-03-24 22:24:54 +0600

    added-fragment-example

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+More on [developer.android](https://developer.android.com/guide/fragments)
+
+
+we are assigning first fragment on xml 
+````xml
+   <fragment android:layout_width="match_parent"
+              android:name="com.example.androidstudiolab.FragmentA"
+              android:layout_height="match_parent"
+              android:id="@+id/fragmentId"
+    />
+````
--- app/src/main/java/com/example/androidstudiolab/MainActivity.java
+++ app/src/main/java/com/example/androidstudiolab/MainActivity.java
@@ -5,14 +5,33 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
     }
 
+    public void handleFragmentChange(View view) {
+
+        if (view.getId() == R.id.btn1) {
+            fragment = new FragmentA();
+        } else if (view.getId() == R.id.btn2) {
+            fragment = new FragmentB();
+        }
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragmentId, fragment);
+        ft.commit();
+    }
 }
